f6f6f1957c78cf01dfd844007a0bcc6b5bbe58e4
jainarsh84 <43778075+jainarsh84@users.noreply.github.com>
2022-01-17 16:23:49 +0530

    commit 3

--- website/profiles/views.py
+++ website/profiles/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_recommendations_view(request):
-  context = {}
-  return render{request,'base_profile.html',context)
+  profile= Profile.objects.get(user=request.user)
+  recs = profile.get_recommended_profiles()
+  
+  context = {'recs',recs}
+  
+  return render{request,'profiles/main.html',context)
